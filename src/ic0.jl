5b5e82ae6d39053069edd150aafaba136dae93c6
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-10-20 16:43:31 -0500

    hotfix

--- src/ic0.jl
+++ src/ic0.jl
@@ -1,3 +1,5 @@
+export ic0
+
 mutable struct NVIDIA_IC0{SM,DM}
   P::SM
   z::DM
@@ -17,13 +19,13 @@ end
 
 for ArrayType in (:(CuVector{T}), :(CuMatrix{T}))
   @eval begin
-    function ldiv!(y::$ArrayType, ic::NVIDIA_IC0{<:$ArrayType,CuSparseMatrixCSR{T,Cint}}, x::$ArrayType) where T <: BlasFloat
+    function ldiv!(y::$ArrayType, ic::NVIDIA_IC0{CuSparseMatrixCSR{T,Cint},<:$ArrayType}, x::$ArrayType) where T <: BlasFloat
       ldiv!(ic.z, LowerTriangular(ic.P), x)   # Forward substitution with L
       ldiv!(y, LowerTriangular(ic.P)', ic.z)  # Backward substitution with Lᴴ
       return y
     end
 
-    function ldiv!(y::$ArrayType, ic::NVIDIA_IC0{<:$ArrayType,CuSparseMatrixCSC{T,Cint}}, x::$ArrayType) where T <: BlasFloat
+    function ldiv!(y::$ArrayType, ic::NVIDIA_IC0{CuSparseMatrixCSC{T,Cint},<:$ArrayType}, x::$ArrayType) where T <: BlasFloat
       ldiv!(ic.z, UpperTriangular(ic.P)', x)  # Forward substitution with L
       ldiv!(y, UpperTriangular(ic.P), ic.z)   # Backward substitution with Lᴴ
       return y
