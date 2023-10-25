021e097af3f3ec598ada5d349b587f3727dc5ff7
Michel Schanen <mschanen@anl.gov>
2023-10-25 13:32:38 -0500

    Fix structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,10 @@
-using LinearAlgebra, SparseArrays, Test
-using KrylovPreconditioners, Krylov
+using AMDGPU
+using CUDA
+using KernelAbstractions
+
+if CUDA.has_cuda_gpu()
+    include("gpu/nvidia.jl")
+end
+if AMDGPU.has_rocm_gpu()
+    include("gpu/amd.jl")
+end
