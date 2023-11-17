561c70c60a9eaab51e53d7e5e8afc9fd778d02fe
Michel Schanen <mschanen@anl.gov>
2023-11-17 12:54:20 -0500

    Fix and tested on Frontier

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,27 @@
-using Krylov
+using AMDGPU
+using CUDA
+using oneAPI
+using Test
+
+@testset "KrylovPreconditioners" begin
+if AMDGPU.functional()
+    @info "Testing AMDGPU backend"
+    @testset "Testing AMDGPU backend" begin
+        include("gpu/amd.jl")
+    end
+end
+
+if CUDA.functional()
+    @info "Testing CUDA backend"
+    @testset "Testing CUDA backend" begin
+        include("gpu/nvidia.jl")
+    end
+end
+
+if oneAPI.functional()
+    @info "Testing oneAPI backend"
+    @testset "Testing oneAPI backend" begin
+        include("gpu/intel.jl")
+    end
+end
+end
