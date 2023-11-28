dee3385b2d9e8b44400895538bec673f4328c057
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-27 23:57:42 -0600

    More fixes

--- ext/CUDA/operators.jl
+++ ext/CUDA/operators.jl
@@ -12,7 +12,8 @@ eltype(A::CUDA_KrylovOperator{T}) where T = T
 size(A::CUDA_KrylovOperator) = (A.m, A.n)
 
 for (SparseMatrixType, BlasType) in ((:(CuSparseMatrixCSR{T}), :BlasFloat),
-                                     (:(CuSparseMatrixCSC{T}), :BlasFloat))
+                                     (:(CuSparseMatrixCSC{T}), :BlasFloat),
+                                     (:(CuSparseMatrixCOO{T}), :BlasFloat)))
     @eval begin
         function KP.KrylovOperator(A::$SparseMatrixType; nrhs::Int=1, transa::Char='N') where T <: $BlasType
             m,n = size(A)
