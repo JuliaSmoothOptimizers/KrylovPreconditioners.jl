d32c75f841379651e7c6b5cb8966dad765461d72
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-27 23:16:19 -0600

    More fixes

--- ext/CUDA/operators.jl
+++ ext/CUDA/operators.jl
@@ -13,33 +13,34 @@ size(A::CUDA_KrylovOperator) = (A.m, A.n)
 
 for (SparseMatrixType, BlasType) in ((:(CuSparseMatrixCSR{T}), :BlasFloat),
                                      (:(CuSparseMatrixCSC{T}), :BlasFloat))
-  @eval begin
-    function KP.KrylovOperator(A::$SparseMatrixType; nrhs::Int=1, transa::Char='N') where T <: $BlasType
-        m,n = size(A)
-        if nrhs == 1
-            alpha = Ref{T}(one(T))
-            beta = Ref{T}(zero(T))
-            descA = CUSPARSE.CuSparseMatrixDescriptor(A, 'O')
-            descX = CUSPARSE.CuDenseVectorDescriptor(T, n)
-            descY = CUSPARSE.CuDenseVectorDescriptor(T, m)
-            algo = CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT
-            buffer_size = Ref{Csize_t}()
-            CUSPARSE.cusparseSpMV_bufferSize(CUDA.handle(), transa, alpha, descA, descX, beta, descY, T, algo, buffer_size)
-            buffer = CuVector{UInt8}(undef, buffer_size)
-            return CUDA_KrylovOperator{T}(m, n, rhs, transa, descA, buffer)
-        else
-            alpha = Ref{T}(one(T))
-            beta = Ref{T}(zeto(T))
-            descA = CUSPARSE.CuSparseMatrixDescriptor(A, 'O')
-            descX = CUSPARSE.CuDenseMatrixDescriptor(T, n, nrhs)
-            descY = CUSPARSE.CuDenseMatrixDescriptor(T, m, nrhs)
-            algo = CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT
-            buffer_size = Ref{Csize_t}()
-            transb = 'N'
-            CUSPARSE.cusparseSpMM_bufferSize(CUDA.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer_size)
-            buffer = CuVector{UInt8}(undef, buffer_size)
-            CUSPARSE.cusparseSpMM_preprocess(CUDA.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer)
-            return CUDA_KrylovOperator{T}(m, n, rhs, transa, descA, buffer)
+    @eval begin
+        function KP.KrylovOperator(A::$SparseMatrixType; nrhs::Int=1, transa::Char='N') where T <: $BlasType
+            m,n = size(A)
+            if nrhs == 1
+                alpha = Ref{T}(one(T))
+                beta = Ref{T}(zero(T))
+                descA = CUSPARSE.CuSparseMatrixDescriptor(A, 'O')
+                descX = CUSPARSE.CuDenseVectorDescriptor(T, n)
+                descY = CUSPARSE.CuDenseVectorDescriptor(T, m)
+                algo = CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT
+                buffer_size = Ref{Csize_t}()
+                CUSPARSE.cusparseSpMV_bufferSize(CUDA.handle(), transa, alpha, descA, descX, beta, descY, T, algo, buffer_size)
+                buffer = CuVector{UInt8}(undef, buffer_size)
+                return CUDA_KrylovOperator{T}(m, n, rhs, transa, descA, buffer)
+            else
+                alpha = Ref{T}(one(T))
+                beta = Ref{T}(zeto(T))
+                descA = CUSPARSE.CuSparseMatrixDescriptor(A, 'O')
+                descX = CUSPARSE.CuDenseMatrixDescriptor(T, n, nrhs)
+                descY = CUSPARSE.CuDenseMatrixDescriptor(T, m, nrhs)
+                algo = CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT
+                buffer_size = Ref{Csize_t}()
+                transb = 'N'
+                CUSPARSE.cusparseSpMM_bufferSize(CUDA.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer_size)
+                buffer = CuVector{UInt8}(undef, buffer_size)
+                CUSPARSE.cusparseSpMM_preprocess(CUDA.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer)
+                return CUDA_KrylovOperator{T}(m, n, rhs, transa, descA, buffer)
+            end
         end
     end
 end
