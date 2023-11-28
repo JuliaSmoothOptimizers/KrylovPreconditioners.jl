6f9fd725ed894e6b985edc38910620288060705f
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-28 00:06:15 -0600

    More fixes

--- ext/CUDA/operators.jl
+++ ext/CUDA/operators.jl
@@ -25,7 +25,7 @@ for (SparseMatrixType, BlasType) in ((:(CuSparseMatrixCSR{T}), :BlasFloat),
                 descY = CUSPARSE.CuDenseVectorDescriptor(T, m)
                 algo = CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT
                 buffer_size = Ref{Csize_t}()
-                CUSPARSE.cusparseSpMV_bufferSize(CUDA.handle(), transa, alpha, descA, descX, beta, descY, T, algo, buffer_size)
+                CUSPARSE.cusparseSpMV_bufferSize(CUSPARSE.handle(), transa, alpha, descA, descX, beta, descY, T, algo, buffer_size)
                 buffer = CuVector{UInt8}(undef, buffer_size)
                 return CUDA_KrylovOperator{T}(m, n, nrhs, transa, descA, buffer)
             else
@@ -37,9 +37,9 @@ for (SparseMatrixType, BlasType) in ((:(CuSparseMatrixCSR{T}), :BlasFloat),
                 algo = CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT
                 buffer_size = Ref{Csize_t}()
                 transb = 'N'
-                CUSPARSE.cusparseSpMM_bufferSize(CUDA.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer_size)
+                CUSPARSE.cusparseSpMM_bufferSize(CUSPARSE.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer_size)
                 buffer = CuVector{UInt8}(undef, buffer_size)
-                CUSPARSE.cusparseSpMM_preprocess(CUDA.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer)
+                CUSPARSE.cusparseSpMM_preprocess(CUSPARSE.handle(), transa, transb, alpha, descA, descX, beta, descY, T, algo, buffer)
                 return CUDA_KrylovOperator{T}(m, n, nrhs, transa, descA, buffer)
             end
         end
@@ -52,7 +52,7 @@ function LinearAlgebra.mul!(y::CuVector{T}, A::CUDA_KrylovOperator{T}, x::CuVect
     (A.nrhs != 1) && throw(DimensionMismatch(""))
     descY = CUSPARSE.CuDenseVectorDescriptor(y)
     descX = CUSPARSE.CuDenseVectorDescriptor(x)
-    CUSPARSE.cusparseSpMV(CUDA.handle(), A.transa, one(T), A.descA, descX, zero(T), descY, T, A.algo, A.buffer)
+    CUSPARSE.cusparseSpMV(CUSPARSE.handle(), A.transa, one(T), A.descA, descX, zero(T), descY, T, A.algo, A.buffer)
 end
 
 function LinearAlgebra.mul!(Y::CuMatrix{T}, A::CUDA_KrylovOperator{T}, X::CuMatrix{T}) where T <: BlasFloat
@@ -63,5 +63,5 @@ function LinearAlgebra.mul!(Y::CuMatrix{T}, A::CUDA_KrylovOperator{T}, X::CuMatr
     (nY == nX == A.nrhs) || throw(DimensionMismatch(""))
     descY = CUSPARSE.CuDenseVectorDescriptor(Y)
     descX = CUSPARSE.CuDenseVectorDescriptor(X)
-    CUSPARSE.cusparseSpMM(CUDA.handle(), A.transa, 'N', one(T), A.descA, descX, zero(T), descY, T, A.algo, A.buffer)
+    CUSPARSE.cusparseSpMM(CUSPARSE.handle(), A.transa, 'N', one(T), A.descA, descX, zero(T), descY, T, A.algo, A.buffer)
 end
