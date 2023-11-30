63f14a1de498edcb618c16dce708ae16011a0795
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-30 15:53:08 -0600

    Update IC(0) and ILU(0) for CUDA GPUs

--- ext/CUDA/ilu0.jl
+++ ext/CUDA/ilu0.jl
@@ -1,15 +1,68 @@
-mutable struct NVIDIA_ILU0{SM} <: AbstractKrylovPreconditioner
-  P::SM
-  timer_update::Float64
+mutable struct ILU0Info
+    info::CUSPARSE.csrilu02Info_t
+
+    function ILU0Info()
+        info_ref = Ref{CUSPARSE.csrilu02Info_t}()
+        CUSPARSE.cusparseCreateCsrilu02Info(info_ref)
+        obj = new(info_ref[])
+        finalizer(CUSPARSE.cusparseDestroyCsrilu02Info, obj)
+        obj
+    end
 end
 
-for (SparseMatrixType, BlasType) in ((:(CuSparseMatrixCSR{T,Cint}), :BlasFloat),
-                                     (:(CuSparseMatrixCSC{T,Cint}), :BlasReal))
+Base.unsafe_convert(::Type{CUSPARSE.csrilu02Info_t}, info::ILU0Info) = info.info
+
+mutable struct NVIDIA_ILU0{SM} <: AbstractKrylovPreconditioner
+  desc::CUSPARSE.cusparseMatDescr_t
+  buffer::CuVector{UInt8}
+  info::ILU0Info
+  timer_update::Float64
+  P::SM
+end
+
+for (bname, aname, sname, T) in ((:cusparseScsrilu02_bufferSize, :cusparseScsrilu02_analysis, :cusparseScsrilu02, :Float32),
+                                 (:cusparseDcsrilu02_bufferSize, :cusparseDcsrilu02_analysis, :cusparseDcsrilu02, :Float64),
+                                 (:cusparseCcsrilu02_bufferSize, :cusparseCcsrilu02_analysis, :cusparseCcsrilu02, :ComplexF32),
+                                 (:cusparseZcsrilu02_bufferSize, :cusparseZcsrilu02_analysis, :cusparseZcsrilu02, :ComplexF64))
   @eval begin
-    function KP.kp_ilu0(A::$SparseMatrixType) where T <: $BlasType
-      P = CUSPARSE.ilu02(A)
-      n = checksquare(A)
-      return NVIDIA_ILU0(P,0.0)
+    function KP.kp_ilu0(A::CuSparseMatrixCSR{$T,Cint})
+      P = copy(A)
+      n = checksquare(P)
+      desc = CUSPARSE.CuMatrixDescriptor('G', 'L', 'N', 'O')
+      info = ILU0Info()
+      buffer_size = Ref{Cint}()
+      CUSPARSE.$bname(CUSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.rowPtr, P.colVal, info, buffer_size)
+      buffer = CuVector{UInt8}(undef, buffer_size[])
+      CUSPARSE.$aname(CUSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.rowPtr, P.colVal, info, CUSPARSE.CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer)
+      posit = Ref{Cint}(1)
+      CUSPARSE.cusparseXcsric02_zeroPivot(CUSPARSE.handle(), info, posit)
+      (posit[] ≥ 0) && error("Structural/numerical zero in A at ($(posit[]),$(posit[])))")
+      CUSPARSE.$sname(CUSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.rowPtr, P.colVal, info, CUSPARSE.CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer)
+      return NVIDIA_ILU0(desc, buffer, info, 0.0, P)
+    end
+
+    function KP.update!(p::NVIDIA_ILU0{CuSparseMatrixCSC{$T,Cint}}, A::CuSparseMatrixCSR{$T,Cint})
+      p.P = CUSPARSE.ilu02(A)
+    end
+
+    function KP.kp_ilu0(A::CuSparseMatrixCSC{$T,Cint})
+      P = copy(A)
+      n = checksquare(P)
+      desc = CUSPARSE.CuMatrixDescriptor('G', 'L', 'N', 'O')
+      info = ILU0Info()
+      buffer_size = Ref{Cint}()
+      CUSPARSE.$bname(CUSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.colPtr, P.rowVal, info, buffer_size)
+      buffer = CuVector{UInt8}(undef, buffer_size[])
+      CUSPARSE.$aname(CUSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.colPtr, P.rowVal, info, CUSPARSE.CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer)
+      posit = Ref{Cint}(1)
+      CUSPARSE.cusparseXcsric02_zeroPivot(CUSPARSE.handle(), info, posit)
+      (posit[] ≥ 0) && error("Structural/numerical zero in A at ($(posit[]),$(posit[])))")
+      CUSPARSE.$sname(CUSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.colPtr, P.rowVal, info, CUSPARSE.CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer)
+      return NVIDIA_ILU0(desc, buffer, info, 0.0, P)
+    end
+
+    function KP.update!(p::NVIDIA_ILU0{CuSparseMatrixCSC{$T,Cint}}, A::CuSparseMatrixCSC{$T,Cint})
+      p.P = CUSPARSE.ilu02(A)
     end
   end
 end
@@ -44,8 +97,4 @@ for ArrayType in (:(CuVector{T}), :(CuMatrix{T}))
       return y
     end
   end
-end
-
-function KP.update!(p::NVIDIA_ILU0, A)
-    p.P = CUSPARSE.ilu02(A)
 end
