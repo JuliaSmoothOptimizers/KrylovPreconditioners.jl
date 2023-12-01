e6a287e4af78ebff2e06c771fc1ced1b910c03d7
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-30 22:03:50 -0600

    [GPU] Update IC(0) and ILU(0) for AMD GPUs

--- ext/AMDGPU/ilu0.jl
+++ ext/AMDGPU/ilu0.jl
@@ -1,18 +1,59 @@
-mutable struct AMD_ILU0{SM}
-  P::SM
+mutable struct AMD_ILU0{SM} <: AbstractKrylovPreconditioner
+  n::Int
+  desc::rocSPARSE.ROCMatrixDescriptor
+  buffer::ROCVector{UInt8}
+  info::MatInfo
   timer_update::Float64
+  P::SM
 end
 
-for (SparseMatrixType, BlasType) in ((:(ROCSparseMatrixCSR{T,Cint}), :BlasFloat),
-                                     (:(ROCSparseMatrixCSC{T,Cint}), :BlasReal))
+for (bname, aname, sname, T) in ((:rocsparse_scsrilu0_buffer_size, :rocsparse_scsrilu0_analysis, :rocsparse_scsrilu0, :Float32),
+                                 (:rocsparse_dcsrilu0_buffer_size, :rocsparse_dcsrilu0_analysis, :rocsparse_dcsrilu0, :Float64),
+                                 (:rocsparse_ccsrilu0_buffer_size, :rocsparse_ccsrilu0_analysis, :rocsparse_ccsrilu0, :ComplexF32),
+                                 (:rocsparse_zcsrilu0_buffer_size, :rocsparse_zcsrilu0_analysis, :rocsparse_zcsrilu0, :ComplexF64))
   @eval begin
-    function KP.kp_ilu0(A::$SparseMatrixType) where T <: $BlasType
-      P = rocSPARSE.ilu0(A, 'O')
-      return AMD_ILU0(P, 0.0)
+    function KP.kp_ilu0(A::ROCSparseMatrixCSR{$T,Cint})
+      P = copy(A)
+      n = checksquare(P)
+      desc = rocSPARSE.ROCMatrixDescriptor('G', 'L', 'N', 'O')
+      info = MatInfo()
+      buffer_size = Ref{Cint}()
+      rocSPARSE.$bname(rocSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.rowPtr, P.colVal, info, buffer_size)
+      buffer = ROCVector{UInt8}(undef, buffer_size[])
+      rocSPARSE.$aname(rocSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.rowPtr, P.colVal, info, rocSPARSE.rocsparse_analysis_policy_force, rocSPARSE.rocsparse_solve_policy_auto, buffer)
+      posit = Ref{Cint}(1)
+      rocSPARSE.rocsparse_csrilu0_zero_pivot(rocSPARSE.handle(), info, posit)
+      (posit[] ≥ 0) && error("Structural/numerical zero in A at ($(posit[]),$(posit[])))")
+      rocSPARSE.$sname(rocSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.rowPtr, P.colVal, info, rocSPARSE.rocsparse_solve_policy_auto, buffer)
+      return AMD_ILU0(n, desc, buffer, info, 0.0, P)
     end
 
-    function KP.update!(p::AMD_ILU0{$SparseMatrixType}, A::$SparseMatrixType) where T <: $BlasType
-      p.P = rocSPARSE.ilu0(A, 'O')
+    function KP.update!(p::AMD_ILU0{ROCSparseMatrixCSR{$T,Cint}}, A::ROCSparseMatrixCSR{$T,Cint})
+      copyto!(p.P.nzVal, A.nzVal)
+      rocSPARSE.$sname(rocSPARSE.handle(), p.n, nnz(p.P), p.desc, p.P.nzVal, p.P.rowPtr, p.P.colVal, p.info, rocSPARSE.rocsparse_solve_policy_auto, p.buffer)
+      return p
+    end
+
+    function KP.kp_ilu0(A::ROCSparseMatrixCSC{$T,Cint})
+      P = copy(A)
+      n = checksquare(P)
+      desc = rocSPARSE.ROCMatrixDescriptor('G', 'L', 'N', 'O')
+      info = MatInfo()
+      buffer_size = Ref{Cint}()
+      rocSPARSE.$bname(rocSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.colPtr, P.rowVal, info, buffer_size)
+      buffer = ROCVector{UInt8}(undef, buffer_size[])
+      rocSPARSE.$aname(rocSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.colPtr, P.rowVal, info, rocSPARSE.rocsparse_analysis_policy_force, rocSPARSE.rocsparse_solve_policy_auto, buffer)
+      posit = Ref{Cint}(1)
+      rocSPARSE.rocsparse_csrilu0_zero_pivot(rocSPARSE.handle(), info, posit)
+      (posit[] ≥ 0) && error("Structural/numerical zero in A at ($(posit[]),$(posit[])))")
+      rocSPARSE.$sname(rocSPARSE.handle(), n, nnz(P), desc, P.nzVal, P.colPtr, P.rowVal, info, rocSPARSE.rocsparse_solve_policy_auto, buffer)
+      return AMD_ILU0(n, desc, buffer, info, 0.0, P)
+    end
+
+    function KP.update!(p::AMD_ILU0{ROCSparseMatrixCSC{$T,Cint}}, A::ROCSparseMatrixCSC{$T,Cint})
+      copyto!(p.P.nzVal, A.nzVal)
+      rocSPARSE.$sname(rocSPARSE.handle(), p.n, nnz(p.P), p.desc, p.P.nzVal, p.P.colPtr, p.P.rowVal, p.info, rocSPARSE.rocsparse_solve_policy_auto, p.buffer)
+      return p
     end
   end
 end
