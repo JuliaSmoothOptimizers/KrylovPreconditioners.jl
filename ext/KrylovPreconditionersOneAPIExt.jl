312f3c38471513dae458835111d6c3334bf6292e
Mosè Giordano <mose@gnu.org>
2025-11-24 21:30:42 +0000

    Make package loadable on systems without oneAPI
    At the moment it's impossible to load this package in an environment with
    `oneAPI.jl`, if oneAPI isn't actually available (e.g. on aarch64 machines),
    which breaks lots of downstream workflows.

--- ext/KrylovPreconditionersOneAPIExt.jl
+++ ext/KrylovPreconditionersOneAPIExt.jl
@@ -1,19 +1,23 @@
 module KrylovPreconditionersOneAPIExt
-using LinearAlgebra
-using SparseArrays
 using oneAPI
-using oneAPI: global_queue, sycl_queue, context, device
-using oneAPI.oneMKL
-using LinearAlgebra: checksquare, BlasReal, BlasFloat
-import LinearAlgebra: ldiv!, mul!
-import Base: size, eltype, unsafe_convert
 
-using KrylovPreconditioners
-const KP = KrylovPreconditioners
-using KernelAbstractions
-const KA = KernelAbstractions
+# Don't do anything if oneAPI isn't actually available.
+if oneAPI.oneL0.NEO_jll.is_available() && oneAPI.oneL0.functional[]
+    using oneAPI: global_queue, sycl_queue, context, device
+    using LinearAlgebra
+    using SparseArrays
+    using oneAPI.oneMKL
+    using LinearAlgebra: checksquare, BlasReal, BlasFloat
+    import LinearAlgebra: ldiv!, mul!
+    import Base: size, eltype, unsafe_convert
 
-include("oneAPI/block_jacobi.jl")
-include("oneAPI/operators.jl")
+    using KrylovPreconditioners
+    const KP = KrylovPreconditioners
+    using KernelAbstractions
+    const KA = KernelAbstractions
+
+    include("oneAPI/block_jacobi.jl")
+    include("oneAPI/operators.jl")
+end
 
 end
